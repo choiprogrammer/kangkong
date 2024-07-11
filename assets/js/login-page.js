ba39bfa6ba97f2b2afb0bb145b76698a13e5cd95
choiprogrammer <133006502+choiprogrammer@users.noreply.github.com>
2024-07-11 14:55:14 +0800

    Update login-page.js

--- assets/js/login-page.js
+++ assets/js/login-page.js
@@ -1,5 +1,5 @@
     document.getElementById("loginForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault();
 
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
